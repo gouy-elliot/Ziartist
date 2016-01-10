eebf72b4925546d4053fdcbcc920059d9aca32cf
gouy_e <elliot.gouy@epitech.eu>
2016-01-10 20:08:54 +0100

    fix some shit

--- API/IHttpResponse.h
+++ API/IHttpResponse.h
@@ -4,7 +4,7 @@
 #include                           <string>
 #include		                       "IHttpObject.h"
 
-namespace		                       Ziartiste
+namespace		                       Ziartist
 {
 
   struct                           HttpStatus
